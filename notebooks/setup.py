cbfd66ae5e32bbc4109454e17ebb7ab7e405864a
sergio <ser.0090@gmail.com>
2020-10-21 00:44:12 -0300

    notebook to read statisctis from api

---
+++ notebooks/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Set-up
+-----------------
+"""
+
+from distutils.core import setup
+
+
+setup(
+    name='ddosdn',
+    version='1.0',
+    description='get statistics',
+    author='ser0090',
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'requests',
+        'ipython',
+        'jupyterlab',
+        'numpy',
+    ]
+)
